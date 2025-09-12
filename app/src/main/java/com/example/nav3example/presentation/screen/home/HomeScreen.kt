bb0ccdd2801e183575dced05c2cad2d45711f90b
Vitor Moraes <vitordmoraes.93@gmail.com>
2025-09-12 11:08:49 -0300

    fixes

--- app/src/main/java/com/example/nav3example/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/nav3example/presentation/screen/home/HomeScreen.kt
@@ -2,10 +2,7 @@ package com.example.nav3example.presentation.screen.home
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -19,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.nav3example.presentation.components.BottomNavigationBar
 import com.example.nav3example.presentation.components.TopBar
 import com.example.nav3example.presentation.navigation.HomeNavigation
